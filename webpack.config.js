b58428cb31d1f1789562f3cc6143e5bf3877550f
Hayden Lin <info@haydenlin.nl>
2020-03-01 14:00:50 +0100

    initial

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const files = [
+    './src/adminer.scss'
+];
+
+module.exports = [
+    {
+        mode: 'development',
+        entry: files,
+        devtool: "source-map",
+        optimization: {
+            minimizer: [
+                new OptimizeCSSAssetsPlugin({})
+            ],
+        },
+        module: {
+
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader?importLoaders=1"
+                    ]
+                },
+                {
+                    test: /\.(sass|scss)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader', options: {
+                                import: false,
+                                sourceMap: true,
+                                importLoaders: 2
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader', options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader', options: {
+                                sourceMap: true,
+                                sassOptions: {
+                                    includePaths: ["node_modules"]
+                                }
+                            }
+                        }
+                    ]
+                },
+            ]
+        },
+        plugins: [
+            new StyleLintPlugin({
+                configFile: '.stylelintrc',
+            }),
+            new MiniCssExtractPlugin({// define where to save the file
+                filename: 'adminer.css',
+                allChunks: true,
+            }),
+        ]
+    },
+]
